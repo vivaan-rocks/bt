155aeac164822d0c9404a361949aecd6b68b0aa4
vivaan-rocks <79306482+vivaan-rocks@users.noreply.github.com>
2021-07-09 21:29:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+var canvas = new fabric.Canvas('myCanvas');
+
+player_x = 10;
+player_y = 10;
+
+block_image_width = 30;
+block_image_height = 30;
+
+var player_object= "";
+var block_image_object= "";
+
+function player_update()
+{
+	fabric.Image.fromURL("https://i.postimg.cc/zDwfFdYY/player.png", function(Img) {
+	player_object = Img;
+
+	player_object.scaleToWidth(150);
+	player_object.scaleToHeight(140);
+	player_object.set({
+	top:player_y,
+	left:player_x
+	});
+	canvas.add(player_object);
+
+	});
+}
+
+function new_image(get_image)
+{
+	fabric.Image.fromURL(get_image, function(Img) {
+	block_image_object = Img;
+
+	block_image_object.scaleToWidth(block_image_width);
+	block_image_object.scaleToHeight(block_image_height);
+	block_image_object.set({
+	top:player_y,
+	left:player_x
+	});
+	canvas.add(block_image_object);
+
+	});
+
+}
+if(keyPressed == '70'){
+	new_image('ironman_face.png');
+	console.log("f");
+}
+if(keyPressed == '66'){
+	new_image('spiderman_body.png');
+	console.log("b");
+}
+if(keyPressed == '76'){
+	new_image('hulk_legs.png');
+	console.log("l");
+}
+if(keyPressed == '82'){
+	new_image('thor_right_hand.png');
+	console.log("r");
+}
+if(keyPressed == '72'){
+	new_image('captain_america_left_hand.png');
+	console.log("h");
+}
+if(keyPressed == '38'){
+	up();
+	console.log("up");
+}
